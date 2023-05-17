9d5b8f0ae8f135af28dfd7ceeb1c13c33009ec3c
Kleinccx <124169356+Kleinccx@users.noreply.github.com>
2023-05-17 20:01:23 +0800

    Add files via upload

---
+++ Activity 1- SQL Commands in Data ManipulationAssignment/DATA MANIPULATION/SQL MANIPULATION/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace SQL_MANIPULATION
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grid1 = new System.Windows.Forms.DataGridView();
+            this.showEmployee = new System.Windows.Forms.Button();
+            this.ttlSalary = new System.Windows.Forms.Button();
+            this.AvgSalaryLnameM = new System.Windows.Forms.Button();
+            this.DisplaySalaryGreaterthanAVGSalaryWithFullName = new System.Windows.Forms.Button();
+            this.DisplayttlSalaryofAllSales = new System.Windows.Forms.Button();
+            this.DisplayPaidLeast = new System.Windows.Forms.Button();
+            this.DisplayAllEmployeesNameDepartmentTrainingCourseAttended = new System.Windows.Forms.Button();
+            this.EMPLOYEEMORETHANONETRAINING = new System.Windows.Forms.Button();
+            this.DisplayAngerMangementTrainingCourse = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.grid1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grid1
+            // 
+            this.grid1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid1.Location = new System.Drawing.Point(116, 12);
+            this.grid1.Name = "grid1";
+            this.grid1.Size = new System.Drawing.Size(570, 236);
+            this.grid1.TabIndex = 3;
+            // 
+            // showEmployee
+            // 
+            this.showEmployee.Location = new System.Drawing.Point(290, 254);
+            this.showEmployee.Name = "showEmployee";
+            this.showEmployee.Size = new System.Drawing.Size(196, 23);
+            this.showEmployee.TabIndex = 4;
+            this.showEmployee.Text = "Display";
+            this.showEmployee.UseVisualStyleBackColor = true;
+            this.showEmployee.Click += new System.EventHandler(this.showEmployee_Click);
+            // 
+            // ttlSalary
+            // 
+            this.ttlSalary.Location = new System.Drawing.Point(53, 303);
+            this.ttlSalary.Name = "ttlSalary";
+            this.ttlSalary.Size = new System.Drawing.Size(196, 23);
+            this.ttlSalary.TabIndex = 5;
+            this.ttlSalary.Text = "Total Salary";
+            this.ttlSalary.UseVisualStyleBackColor = true;
+            this.ttlSalary.Click += new System.EventHandler(this.ttlSalary_Click);
+            // 
+            // AvgSalaryLnameM
+            // 
+            this.AvgSalaryLnameM.Location = new System.Drawing.Point(53, 332);
+            this.AvgSalaryLnameM.Name = "AvgSalaryLnameM";
+            this.AvgSalaryLnameM.Size = new System.Drawing.Size(196, 23);
+            this.AvgSalaryLnameM.TabIndex = 6;
+            this.AvgSalaryLnameM.Text = "Average Salary ";
+            this.AvgSalaryLnameM.UseVisualStyleBackColor = true;
+            this.AvgSalaryLnameM.Click += new System.EventHandler(this.AvgSalaryLnameM_Click);
+            // 
+            // DisplaySalaryGreaterthanAVGSalaryWithFullName
+            // 
+            this.DisplaySalaryGreaterthanAVGSalaryWithFullName.Location = new System.Drawing.Point(53, 361);
+            this.DisplaySalaryGreaterthanAVGSalaryWithFullName.Name = "DisplaySalaryGreaterthanAVGSalaryWithFullName";
+            this.DisplaySalaryGreaterthanAVGSalaryWithFullName.Size = new System.Drawing.Size(196, 23);
+            this.DisplaySalaryGreaterthanAVGSalaryWithFullName.TabIndex = 7;
+            this.DisplaySalaryGreaterthanAVGSalaryWithFullName.Text = "Display Salary Greater than AVG salary";
+            this.DisplaySalaryGreaterthanAVGSalaryWithFullName.UseVisualStyleBackColor = true;
+            this.DisplaySalaryGreaterthanAVGSalaryWithFullName.Click += new System.EventHandler(this.DisplaySalaryGreaterthanAVGSalaryWithFullName_Click);
+            // 
+            // DisplayttlSalaryofAllSales
+            // 
+            this.DisplayttlSalaryofAllSales.Location = new System.Drawing.Point(53, 390);
+            this.DisplayttlSalaryofAllSales.Name = "DisplayttlSalaryofAllSales";
+            this.DisplayttlSalaryofAllSales.Size = new System.Drawing.Size(196, 23);
+            this.DisplayttlSalaryofAllSales.TabIndex = 8;
+            this.DisplayttlSalaryofAllSales.Text = "Total Salaries of all SALES";
+            this.DisplayttlSalaryofAllSales.UseVisualStyleBackColor = true;
+            this.DisplayttlSalaryofAllSales.Click += new System.EventHandler(this.DisplayttlSalaryofAllSales_Click);
+            // 
+            // DisplayPaidLeast
+            // 
+            this.DisplayPaidLeast.Location = new System.Drawing.Point(513, 303);
+            this.DisplayPaidLeast.Name = "DisplayPaidLeast";
+            this.DisplayPaidLeast.Size = new System.Drawing.Size(214, 23);
+            this.DisplayPaidLeast.TabIndex = 9;
+            this.DisplayPaidLeast.Text = "Paid Least";
+            this.DisplayPaidLeast.UseVisualStyleBackColor = true;
+            this.DisplayPaidLeast.Click += new System.EventHandler(this.DisplayPaidLeast_Click);
+            // 
+            // DisplayAllEmployeesNameDepartmentTrainingCourseAttended
+            // 
+            this.DisplayAllEmployeesNameDepartmentTrainingCourseAttended.Location = new System.Drawing.Point(513, 332);
+            this.DisplayAllEmployeesNameDepartmentTrainingCourseAttended.Name = "DisplayAllEmployeesNameDepartmentTrainingCourseAttended";
+            this.DisplayAllEmployeesNameDepartmentTrainingCourseAttended.Size = new System.Drawing.Size(214, 23);
+            this.DisplayAllEmployeesNameDepartmentTrainingCourseAttended.TabIndex = 10;
+            this.DisplayAllEmployeesNameDepartmentTrainingCourseAttended.Text = "ALL EMPLOYEES";
+            this.DisplayAllEmployeesNameDepartmentTrainingCourseAttended.UseVisualStyleBackColor = true;
+            this.DisplayAllEmployeesNameDepartmentTrainingCourseAttended.Click += new System.EventHandler(this.DisplayAllEmployeesNameDepartmentTrainingCourseAttended_Click);
+            // 
+            // EMPLOYEEMORETHANONETRAINING
+            // 
+            this.EMPLOYEEMORETHANONETRAINING.Location = new System.Drawing.Point(513, 361);
+            this.EMPLOYEEMORETHANONETRAINING.Name = "EMPLOYEEMORETHANONETRAINING";
+            this.EMPLOYEEMORETHANONETRAINING.Size = new System.Drawing.Size(214, 23);
+            this.EMPLOYEEMORETHANONETRAINING.TabIndex = 11;
+            this.EMPLOYEEMORETHANONETRAINING.Text = "EMPLOYEE MORE THAN ONE TRAINING";
+            this.EMPLOYEEMORETHANONETRAINING.UseVisualStyleBackColor = true;
+            this.EMPLOYEEMORETHANONETRAINING.Click += new System.EventHandler(this.EMPLOYEEMORETHANONETRAINING_Click);
+            // 
+            // DisplayAngerMangementTrainingCourse
+            // 
+            this.DisplayAngerMangementTrainingCourse.Location = new System.Drawing.Point(513, 390);
+            this.DisplayAngerMangementTrainingCourse.Name = "DisplayAngerMangementTrainingCourse";
+            this.DisplayAngerMangementTrainingCourse.Size = new System.Drawing.Size(214, 23);
+            this.DisplayAngerMangementTrainingCourse.TabIndex = 12;
+            this.DisplayAngerMangementTrainingCourse.Text = "DISPLAY ANGER MANAGEMENT";
+            this.DisplayAngerMangementTrainingCourse.UseVisualStyleBackColor = true;
+            this.DisplayAngerMangementTrainingCourse.Click += new System.EventHandler(this.DisplayAngerMangementTrainingCourse_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DisplayAngerMangementTrainingCourse);
+            this.Controls.Add(this.EMPLOYEEMORETHANONETRAINING);
+            this.Controls.Add(this.DisplayAllEmployeesNameDepartmentTrainingCourseAttended);
+            this.Controls.Add(this.DisplayPaidLeast);
+            this.Controls.Add(this.DisplayttlSalaryofAllSales);
+            this.Controls.Add(this.DisplaySalaryGreaterthanAVGSalaryWithFullName);
+            this.Controls.Add(this.AvgSalaryLnameM);
+            this.Controls.Add(this.ttlSalary);
+            this.Controls.Add(this.showEmployee);
+            this.Controls.Add(this.grid1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.grid1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView grid1;
+        private System.Windows.Forms.Button showEmployee;
+        private System.Windows.Forms.Button ttlSalary;
+        private System.Windows.Forms.Button AvgSalaryLnameM;
+        private System.Windows.Forms.Button DisplaySalaryGreaterthanAVGSalaryWithFullName;
+        private System.Windows.Forms.Button DisplayttlSalaryofAllSales;
+        private System.Windows.Forms.Button DisplayPaidLeast;
+        private System.Windows.Forms.Button DisplayAllEmployeesNameDepartmentTrainingCourseAttended;
+        private System.Windows.Forms.Button EMPLOYEEMORETHANONETRAINING;
+        private System.Windows.Forms.Button DisplayAngerMangementTrainingCourse;
+    }
+}
+
---
+++ Activity 1- SQL Commands in Data ManipulationAssignment/DATA MANIPULATION/SQL MANIPULATION/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQL_MANIPULATION
+{
+    public partial class Form1 : Form
+    {
+        private OleDbConnection thisConnection;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            thisConnection = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=\"C:\\Users\\Klein\\Desktop\\Activity 1- SQL Commands in Data ManipulationAssignment\\DATA MANIPULATION\\SQL MANIPULATION\\KOMPANYA.accdb\"");
+
+        }
+
+        private void showEmployee_Click(object sender, EventArgs e)
+        {
+
+
+            string sql = "SELECT EMPLOYEEIDNO, EMPLOYEEFIRSTNAME + ' ' + EMPLOYEELASTNAME as FULLNAME, EMPLOYEEDEPARTMENT, EMPLOYEESALARY FROM EMPLOYEEFILE WHERE EMPLOYEESALARY > (SELECT AVG(EMPLOYEESALARY) FROM EMPLOYEEFILE WHERE EMPLOYEEDEPARTMENT = 'PRODUCTION')";
+            OleDbDataAdapter adapter = new OleDbDataAdapter(sql, thisConnection);
+            DataTable dataTable = new DataTable();
+            thisConnection.Open();
+            adapter.Fill(dataTable);
+            grid1.DataSource = dataTable;
+            thisConnection.Close();
+
+
+        }
+
+    
+
+        private void ttlSalary_Click(object sender, EventArgs e)
+        {
+    
+            string sql = "SELECT SUM(EMPLOYEESALARY) FROM EMPLOYEEFILE";
+
+                OleDbCommand command = new OleDbCommand(sql, thisConnection);
+                thisConnection.Open();
+                object result = command.ExecuteScalar();
+                if (result != null && result != DBNull.Value)
+                {
+                    decimal totalSalaries = Convert.ToInt32(result);
+                    MessageBox.Show(string.Format("Total Salary = {0:C}", totalSalaries));
+                }
+                thisConnection.Close();
+             
+            }
+
+        private void AvgSalaryLnameM_Click(object sender, EventArgs e)
+        {
+            string sql = "SELECT AVG(EMPLOYEESALARY) FROM EMPLOYEEFILE WHERE EMPLOYEELASTNAME LIKE 'M%'";
+
+            OleDbCommand command = new OleDbCommand(sql, thisConnection);
+            thisConnection.Open();
+            object result = command.ExecuteScalar();
+            if (result != null && result != DBNull.Value)
+            {
+                decimal avgSalaryStartsM = Convert.ToInt32(result);
+                MessageBox.Show(string.Format("Average Salary of Employees With Lastname That Starts With Letter M = {0:C}", avgSalaryStartsM));
+            }
+            thisConnection.Close();
+
+        }
+
+        private void DisplaySalaryGreaterthanAVGSalaryWithFullName_Click(object sender, EventArgs e)
+        {
+
+            string sql = "SELECT EMPLOYEEIDNO as [ID NUMBER], EMPLOYEEFIRSTNAME as FIRSTNAME, EMPLOYEELASTNAME as LASTNAME, EMPLOYEESALARY as SALARY FROM EMPLOYEEFILE WHERE EMPLOYEEDEPARTMENT = 'PRODUCTION' AND EMPLOYEESALARY > (SELECT AVG(EMPLOYEESALARY) FROM EMPLOYEEFILE WHERE EMPLOYEEDEPARTMENT = 'PRODUCTION')";
+            OleDbDataAdapter adapter = new OleDbDataAdapter(sql, thisConnection);
+            DataTable dataTable = new DataTable();
+            thisConnection.Open();
+            adapter.Fill(dataTable);
+            grid1.DataSource = dataTable;
+            thisConnection.Close();
+        }
+
+        private void DisplayttlSalaryofAllSales_Click(object sender, EventArgs e)
+        {
+            string sql = "SELECT SUM(EMPLOYEESALARY) FROM EMPLOYEEFILE WHERE EMPLOYEEDEPARTMENT = 'SALES'";
+
+            OleDbCommand command = new OleDbCommand(sql, thisConnection);
+            thisConnection.Open();
+            object result = command.ExecuteScalar();
+            if (result != null && result != DBNull.Value)
+            {
+                int totalSalesDepartment = Convert.ToInt32(result);
+                MessageBox.Show(string.Format("Total Sales Department = {0:C}", totalSalesDepartment));
+            }
+            thisConnection.Close();
+        }
+
+        private void DisplayPaidLeast_Click(object sender, EventArgs e)
+        {
+            string sql = "SELECT EMPLOYEEIDNO as [ID Number], EMPLOYEEFIRSTNAME + ' ' + EMPLOYEELASTNAME as [Full Name], EMPLOYEESALARY as Salary, EMPLOYEEDEPARTMENT as Department FROM EMPLOYEEFILE WHERE EMPLOYEESALARY = (SELECT MIN(EMPLOYEESALARY) FROM EMPLOYEEFILE)";
+
+            OleDbDataAdapter adapter = new OleDbDataAdapter(sql, thisConnection);
+            DataTable dataTable = new DataTable();
+            thisConnection.Open();
+            adapter.Fill(dataTable);
+            grid1.DataSource = dataTable;
+            thisConnection.Close();
+
+
+        }
+
+        private void DisplayAllEmployeesNameDepartmentTrainingCourseAttended_Click(object sender, EventArgs e)
+        {
+            string sql = "SELECT EMPLOYEEFILE.EMPLOYEEIDNO, EMPLOYEEFILE.EMPLOYEEFIRSTNAME, EMPLOYEEFILE.EMPLOYEELASTNAME, TRAININGFILE.TRAININGCOURSE, EMPLOYEEFILE.EMPLOYEEDEPARTMENT FROM ((EMPLOYEEFILE INNER JOIN ATTENDANCEFILE ON EMPLOYEEFILE.EMPLOYEEIDNO = ATTENDANCEFILE.ATTENDANCETRAININGEMPLOYEEID) INNER JOIN TRAININGFILE ON ATTENDANCEFILE.ATTENDANCETRAININGCODE = TRAININGFILE.TRAININGCODE)";
+
+            OleDbDataAdapter adapter = new OleDbDataAdapter(sql, thisConnection);
+
+            DataTable table = new DataTable();
+
+         
+            adapter.Fill(table);
+
+        
+            grid1.DataSource = table;
+
+            // Set the column names in the DataGridView control to match the columns provided
+            grid1.Columns[0].HeaderText = "EMPLOYEEIDNO";
+            grid1.Columns[1].HeaderText = "EMPLOYEEFIRSTNAME";
+            grid1.Columns[2].HeaderText = "TRAININGCOURSE";
+            grid1.Columns[3].HeaderText = "EMPLOYEEDEPARTMENT";
+
+        }
+  
+
+        private void EMPLOYEEMORETHANONETRAINING_Click(object sender, EventArgs e)
+        {
+
+            string sql = "SELECT EMPLOYEELASTNAME as [Last Name], COUNT(*) as [Training Count] FROM ATTENDANCEFILE INNER JOIN EMPLOYEEFILE ON ATTENDANCEFILE.ATTENDANCETRAININGEMPLOYEEID = EMPLOYEEFILE.EMPLOYEEIDNO GROUP BY EMPLOYEELASTNAME HAVING COUNT(*) > 1";
+
+            OleDbDataAdapter adapter = new OleDbDataAdapter(sql, thisConnection);
+            DataTable dataTable = new DataTable();
+            thisConnection.Open();
+            adapter.Fill(dataTable);
+            grid1.DataSource = dataTable;
+            thisConnection.Close();
+        }
+
+        private void DisplayAngerMangementTrainingCourse_Click(object sender, EventArgs e)
+        {
+            string sql = "SELECT EMPLOYEEFILE.EMPLOYEEFIRSTNAME + ' ' + EMPLOYEEFILE.EMPLOYEELASTNAME AS [Full Name], EMPLOYEEFILE.EMPLOYEEDEPARTMENT AS [Department] " +
+                                    "FROM ((ATTENDANCEFILE " +
+                                    "INNER JOIN EMPLOYEEFILE ON ATTENDANCEFILE.ATTENDANCETRAININGEMPLOYEEID = EMPLOYEEFILE.EMPLOYEEIDNO) " +
+                                    "INNER JOIN TRAININGFILE ON ATTENDANCEFILE.ATTENDANCETRAININGCODE = TRAININGFILE.TRAININGCODE) " +
+                                    "WHERE TRAININGFILE.TRAININGCOURSE = 'ANGER MANAGEMENT'";
+            OleDbDataAdapter adapter = new OleDbDataAdapter(sql, thisConnection);
+            DataTable dataTable = new DataTable();
+            thisConnection.Open();
+            adapter.Fill(dataTable);
+            grid1.DataSource = dataTable;
+            thisConnection.Close();
+
+        }
+    }
+    
+}
+
